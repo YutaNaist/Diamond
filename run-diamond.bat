1b4711859fb9b91bb1bec032635df9f91de4d3a3
YutaNaist <yuta.yamamoto@ms.naist.jp>
2023-09-27 09:37:16 +0900

    0.0.06 push commit

--- run-diamond.bat
+++ run-diamond.bat
@@ -1,5 +1,5 @@
 @echo off
-start /B python.exe "C:/Program Files/diamond/periodicRun_main.py"
-start /B python.exe "C:/Program Files/diamond/diamond_main.py"
-@REM start /B python.exe "C:/share/Diamond/periodicRun_main.py"
-@REM start /B python.exe "C:/share/Diamond/diamond_main.py"
+@REM start /B python.exe "C:/Program Files/diamond/periodicRun_main.py"
+@REM start /B python.exe "C:/Program Files/diamond/diamond_main.py"
+start /B python.exe "C:/share/Diamond/periodicRun_main.py"
+start /B python.exe "C:/share/Diamond/diamond_main.py"
