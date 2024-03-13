51d7b4c819d454d8d8ebc57d846b52c5940afd42
YutaNaist <yuta.yamamoto@ms.naist.jp>
2024-03-13 19:02:48 +0900

    0.1.00 Phase 1 release

---
+++ readme.md
@@ -0,0 +1,2 @@
+動作確認した環境
+Python  version ---
--- run-diamond.bat
+++ run-diamond.bat
@@ -3,3 +3,4 @@ start  python.exe "C:/Program Files/diamond/periodicRun_main.py"
 start  python.exe "C:/Program Files/diamond/diamond_main.py"
 @REM start /B python.exe "C:/share/Diamond/periodicRun_main.py"
 @REM start /B python.exe "C:/share/Diamond/diamond_main.py"
+
